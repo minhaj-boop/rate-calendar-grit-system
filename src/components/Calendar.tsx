fb3b30be6186cad5e2eae91636bf4bf6700e351b
minhaj-boop <imminhajj@gmail.com>
2024-06-27 15:12:54 +0600

    initial setup of table

--- src/components/Calendar.tsx
+++ src/components/Calendar.tsx
@@ -18,27 +18,33 @@ import "./Calendar.css";
 
 //types
 interface IRoomInventoryCalendar {
-  status: boolean;
+  id: string;
+  date: string;
   available: number;
+  status: boolean;
   booked: number;
 }
 
 interface IRateCalendar {
+  id: string;
+  date: string;
   rate: number;
   min_length_of_stay: number;
   reservation_deadline: number;
 }
 
 interface IRatePlan {
+  id: string;
   name: string;
-  rateCalendar: IRateCalendar[];
+  calendar: IRateCalendar[];
 }
 
 interface IRoomCategory {
+  id: string;
   name: string;
   occupancy: number;
-  inventory: IRoomInventoryCalendar[];
-  ratePlans: IRatePlan[];
+  inventory_calendar: IRoomInventoryCalendar[];
+  rate_plans: IRatePlan[];
 }
 
 interface ApiResponse {
@@ -93,12 +99,17 @@ const Calendar = () => {
     fetchData();
   }, [startValue, endValue]);
 
+  //dummy error and load handling
   if (loading) return <p>Loading...</p>;
   if (error) return <p>Error: {error.message}</p>;
   if (data) {
     console.log(data);
   }
 
+  //comment
+  const uniqueMonths = new Set();
+
+  //dummy
   const rows = [
     createData("Frozen yoghurt", 159, 6.0, 24, 4.0),
     createData("Ice cream sandwich", 237, 9.0, 37, 4.3),
@@ -148,62 +159,33 @@ const Calendar = () => {
 
       <Box sx={{ mt: 30 }}>
         <TableContainer component={Paper} sx={{ display: "flex" }}>
-          {/* <Table
-            sx={{ width: 400, overflowX: "clip" }}
-            aria-label="simple table"
-          >
-            <TableHead>
-              <TableRow>
-                <TableCell
-                  sx={{
-                    borderRight: 1,
-                    borderColor: "grey.200",
-                    height: 15,
-                    maxHeight: 15,
-                  }}
-                >
-                  Dessert (100g serving)
-                </TableCell>
-              </TableRow>
-            </TableHead>
-            <TableBody>
-              {cols.map((col) => (
-                <TableRow
-                  key={col.name}
-                  sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
-                >
-                  <TableCell
-                    sx={{
-                      borderRight: 1,
-                      borderColor: "grey.200",
-                      height: 15,
-                      maxHeight: 15,
-                    }}
-                    component="th"
-                    scope="row"
-                  >
-                    {col.name}
-                  </TableCell>
-                </TableRow>
-              ))}
-            </TableBody>
-          </Table> */}
           <Table
             sx={{ minWidth: 650, overflowX: "scroll" }}
             aria-label="simple table"
           >
             <TableHead>
               <TableRow>
-                <TableCell
-                  sx={{
-                    borderRight: 1,
-                    borderColor: "grey.200",
-                    height: 15,
-                    maxHeight: 15,
-                  }}
-                >
-                  Dessert (100g serving)
-                </TableCell>
+                {data[0]?.inventory_calendar
+                  ?.filter((item) => {
+                    const monthYear = dayjs(item.date).format("MMM YYYY");
+                    if (uniqueMonths.has(monthYear)) {
+                      return false;
+                    }
+                    uniqueMonths.add(monthYear);
+                    return true;
+                  })
+                  .map((item) => (
+                    <TableCell
+                      sx={{
+                        borderRight: 1,
+                        borderColor: "grey.200",
+                        height: 15,
+                        maxHeight: 15,
+                      }}
+                    >
+                      {dayjs(item.date).format("MMM YYYY")}
+                    </TableCell>
+                  ))}
               </TableRow>
               <TableRow>
                 <TableCell
